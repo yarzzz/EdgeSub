58aa558bb205dc5c031e7ed914a52b9c312ffc69
yarzzz <37573826+yarzzz@users.noreply.github.com>
2024-10-02 11:06:12 +0800

    Update share-link.js

--- functions/internal/Parsers/share-link.js
+++ functions/internal/Parsers/share-link.js
@@ -57,7 +57,7 @@ export class ShareLinkParser {
             __Remark: URIObject.fragment || URIObject.host,
             Auth: URIObject.username,
             Hostname: URIObject.hostname,
-            Ports: URIObject.port,
+            Ports: URIObject.ports,
             Query: URIObject.searchParams
         }
         return HY2;
